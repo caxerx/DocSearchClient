67d8259ddc74c86ad000cdfb7db442f86b7716cc
ho tak au yeung <ikouhaha888@gmail.com>
2019-01-13 16:50:33 +0800

    update doc list

--- src/pages/DoctorList.vue
+++ src/pages/DoctorList.vue
@@ -1,36 +1,29 @@
 <template>
-    <div id="app">
-  <v-app id="inspire">
+<v-container>
+  <v-card>
+    <container>
+      <search slot="content"/>
+    </container>
+  </v-card>
+
+
+    <v-card style="margin-top: 50px">
       <container>
-        <search slot="content"/>
+      <v-list slot="content">
+        <v-list-tile v-for="item in items" :key="item.title" @click>
+          <v-list-tile-action>
+            <v-icon v-if="item.icon" color="pink">star</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
       </container>
-  <container>      
-          <v-list slot="content">
-            <v-list-tile
-              v-for="item in items"
-              :key="item.title"
-              @click=""
-              
-            >
-              <v-list-tile-action>
-                <v-icon
-                  v-if="item.icon"
-                  color="pink"
-                >
-                  star
-                </v-icon>
-              </v-list-tile-action>
-  
-              <v-list-tile-content>
-                <v-list-tile-title v-text="item.title"></v-list-tile-title>
-              </v-list-tile-content>
-  
-             
-            </v-list-tile>
-          </v-list>
-  </container>
-  </v-app>
-</div>
+    </v-card>
+
+</v-container>
 </template>
 
 
@@ -39,20 +32,31 @@ import Container from "@/components/Container";
 import Search from "@/components/Search.vue";
 
 export default {
-     el: '#app',
-  data () {
+  data() {
     return {
       items: [
-        { title: 'Dr. Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
-        { title: 'Dr. Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
-        { title: 'Dr. Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
-        { title: 'Dr. Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' }
+        {
+          title: "Dr. Jason Oner",
+          avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"
+        },
+        {
+          title: "Dr. Travis Howard",
+          avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg"
+        },
+        {
+          title: "Dr. Ali Connors",
+          avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg"
+        },
+        {
+          title: "Dr. Cindy Baker",
+          avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg"
+        }
       ]
-    }
+    };
   },
   components: {
     Container,
     Search
-  },
-}
+  }
+};
 </script>
