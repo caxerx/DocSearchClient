a42bf98d0a92192e5fcfd52d54158a5ab3c030be
ho tak au yeung <ikouhaha888@gmail.com>
2019-01-19 21:52:55 +0800

    update reservation list

--- src/components/viewReservation/List.vue
+++ src/components/viewReservation/List.vue
@@ -4,8 +4,17 @@
     <v-card v-for="(content, index) in contents" :key="index" style="margin-bottom:2%">
       <v-card-title primary-title>
         <v-layout row wrap>
-          <v-flex sm1></v-flex>
-          <v-flex sm8 >
+          <v-flex sm1>
+            <v-layout align-center justify-center row fill-height>
+              <v-card color="blue-grey lighten-5">
+                  <div id="dateDiv">
+                 <div class="headline text-md-center"> {{getDay(content.date)}} </div>
+                 <span class="headline grey--text"> {{getMonth(content.date)}} </span>
+                  </div>
+                  </v-card>
+            </v-layout>
+          </v-flex>
+          <v-flex sm8>
             <div class="headline">{{content.doctor}}</div>
             <div class="grey--text">{{content.date}}, {{content.startTime}}</div>
             <div v-if="content.status==='Waiting'" class="error--text">{{content.status}}</div>
@@ -15,13 +24,8 @@
           </v-flex>
           <v-flex sm1 offset-sm1>
             <v-spacer/>
-             <v-btn  v-if="content.status==='Cancel'" disabled>
-               Cancel 
-            </v-btn>
-            <v-btn v-else color="error" @click="changeShow(content)">
-               Cancel 
-            </v-btn>
-            
+            <v-btn v-if="content.status==='Cancel'" disabled>Cancel</v-btn>
+            <v-btn v-else color="error" @click="changeShow(content)">Cancel</v-btn>
           </v-flex>
         </v-layout>
       </v-card-title>
@@ -35,7 +39,8 @@ export default {
   name: "App",
   data() {
     return {
-      search: ""
+      search: "",
+      date: null
     };
   },
   components: {},
@@ -53,6 +58,18 @@ export default {
     ...mapActions(["actionSetShowForViewReservation"]),
     changeShow(content) {
       this.actionSetShowForViewReservation(content);
+    },
+    getDay(date) {
+      //   this.date = new Date(date);
+      //   var d = new Date(this.getter.contents.date);
+      let d = new Date(date);
+    //   console.log(d.toLocaleString('en-us', { month: 'long' }));
+      return d.getDate();
+    },
+    getMonth(date){
+        let d = new Date(date);
+        let month = d.toLocaleString('en-us', { month: 'short' });
+        return month;
     }
   }
 };
@@ -71,5 +88,10 @@ export default {
 #layout {
   /* height:32em; */
   /* overflow-y:scroll; */
+}
+
+#dateDiv{
+    padding-left:5px;
+    padding-right:5px;
 }
 </style>
