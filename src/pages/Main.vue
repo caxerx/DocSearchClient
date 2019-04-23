d964d3b484daa091a95cfe7d9bd7b69e55f749d9
Michael <m11215730@gmail.com>
2019-04-23 18:23:55 +0800

    main.vue, font, pagination

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -10,7 +10,7 @@
       <container>
         <v-flex slot="content" id="medicalInfoTitle" class="display-3">Medical Information</v-flex>
         <v-flex slot="content">
-          <v-flex v-for="item in items" :key="item">
+          <v-flex v-for="item in items" :key="item" style="padding-bottom: 20px">
             <v-layout>
               <v-flex xs2>
                 <v-img
@@ -19,20 +19,26 @@
                   contain
                 ></v-img>
               </v-flex>
-              <v-flex xs7>
+              <v-flex xs10>
                 <v-card-title primary-title style="padding-top: 0px">
                   <div>
-                    <div class="display-1" style="padding-bottom : 10px">What is Lorem Ipsum?</div>
+                    <div
+                      class="display-1 font-weight-medium"
+                      style="padding-bottom : 10px"
+                    >What is Lorem Ipsum?</div>
                     <div
                       class="headline"
                       style="padding-bottom : 10px"
                     >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-                    <div
-                      style="padding-bottom : 30px"
-                    >It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages</div>
+                    <div>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages</div>
                   </div>
                 </v-card-title>
               </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex>
+            <v-layout justify-center>
+              <v-pagination v-model="page" :length="6"></v-pagination>
             </v-layout>
           </v-flex>
         </v-flex>
@@ -51,14 +57,11 @@ export default {
     Search,
     Container
   },
-  data(){
-    return{
-      items:[
-        {key:1},
-        {key:2},
-        {key:3},
-      ]
-    }
+  data() {
+    return {
+      items: [{ key: 1 }, { key: 2 }, { key: 3 }],
+      page: 1
+    };
   }
 };
 </script>
