20f584191b709de1e0cb6458eea2ffe2c6f8a74e
ho tak au yeung <ikouhaha888@gmail.com>
2019-04-24 17:03:30 +0800

    update about us

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -11,25 +11,22 @@
         img="https://www.website.com//img/skin/aboutus/aboutUs_hero.png"
         style="height:700px"
       >
-        <container>
-          <v-flex slot="content" offset-xs1>
+        <v-layout justify-center>
+          <v-flex sm8>
             <v-card-text class="display-1 font-weight-light">
               DocSearch provide a platform for people to search for medical information such as doctor information and health information, and efficient way to book their services.
               Also, we provide a brand new way to see doctor, Online consultation which can provide a more convenient way to do the consultation.
             </v-card-text>
           </v-flex>
-        </container>
+        </v-layout>
       </v-card>
     </v-card>
   </div>
 </template>
 
 <script>
-import Container from "@/components/Container";
 export default {
-  components: {
-    Container
-  }
+  components: {}
 };
 </script>
 
